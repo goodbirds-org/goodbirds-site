a1f80af2c608c900766ef57a06874623ddce3774
goodbirds-org <228260009+goodbirds-org@users.noreply.github.com>
2025-12-17 15:57:54 -0500

    Update build_map.py

--- scripts/build_map.py
+++ scripts/build_map.py
@@ -369,7 +369,7 @@ def save_and_publish(m, outfile: str):
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-NYEBPC2JEZ"></script>
             <script>
                 window.dataLayer = window.dataLayer || [];
-                function gtag(){{dataLayer.push(arguments);}}
+                function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-NYEBPC2JEZ');
             </script>
